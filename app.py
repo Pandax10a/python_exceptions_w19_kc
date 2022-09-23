90bccc17dc67f9b734149da4569c543d3f740497
panda1 <blueblue0011@gmail.com>
2022-09-22 19:36:30 -0700

    kinda done

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 
+from math import nan, pi
+
+
 try:
     string_to_number = 'hi this is string' + 22
 except TypeError:
@@ -8,3 +11,28 @@ except:
 finally:
     string_to_number = "hi this is a string " + '22'
     print(string_to_number)
+
+
+def get_user_input():
+    user_num = input('enter a number: ')
+    try:
+        float(user_num)
+    except ValueError:
+        user_num = input('try again this time a number: ')
+    except:
+        print('just enter a simple number')
+    finally:
+        if(user_num == nan):
+            user_num = pi
+            print('i have chosen for you, the number is pi')
+    result_outside_saved.append(user_num)
+    return user_num
+
+result_outside_saved = []
+
+get_user_input()
+get_user_input()
+   
+sum_result = float(result_outside_saved[0]) + float(result_outside_saved[1])
+
+print(sum_result)
